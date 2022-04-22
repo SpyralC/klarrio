a89298eedd53d2234c730cc70cae15f8a5ee802a
SpyralC <aszln5766@gmail.com>
2022-04-23 01:45:00 +1000

    add script

--- scripts/mysqlmon.sh
+++ scripts/mysqlmon.sh
@@ -8,7 +8,7 @@ sudo service mysql status > /dev/null 2>&1
 if [ $? != 0 ]; then
     echo -e "MySQL Service was down. Restarting now...\n"
     sudo service mysql restart
-    sudo kill -9 `sudo lsof -t -i:9001`
+    sudo kill -9 `sudo lsof -t -i:5000`
 else
     echo -e "MySQL Service is running already. Nothing to do here.\n"
 fi
