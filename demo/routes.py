5e00dfa3653056b4d6793fe5faeb4143448deffc
SpyralC <aszln5766@gmail.com>
2022-04-23 12:04:31 +1000

    add script

--- demo/routes.py
+++ demo/routes.py
@@ -22,28 +22,42 @@ class SubmitForm(FlaskForm):
 @app.route('/', methods=["POST", "GET"])
 def index():
     form = SubmitForm()
-    if form.validate_on_submit():
-        
-        cur = conn.cursor()
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            
+            cur = conn.cursor()
 
-        sql = "INSERT INTO `peopleAge` (`name`, `age`) VALUES (%s, %s)"
-        val = (form.name.data,form.age.data)
-        cur.execute(sql,val)
-        conn.commit()
-        flash('name: {}, age: {} has been submitted successfully'.format(form.name.data, form.age.data), 'success')
+            sql = "INSERT INTO `peopleAge` (`name`, `age`) VALUES (%s, %s)"
+            val = (form.name.data,form.age.data)
+            cur.execute(sql,val)
+            conn.commit()
+            flash('name: {}, age: {} has been submitted successfully'.format(form.name.data, form.age.data), 'success')
 
-        sql = "select * from peopleAge"
-        cur.execute(sql)
-        content = cur.fetchall()
+            sql = "select * from peopleAge"
+            cur.execute(sql)
+            content = cur.fetchall()
 
-        sql = "SHOW FIELDS FROM peopleAge"
-        cur.execute(sql)
-        labels = cur.fetchall()
-        labels = [l[0] for l in labels]
+            sql = "SHOW FIELDS FROM peopleAge"
+            cur.execute(sql)
+            labels = cur.fetchall()
+            labels = [l[0] for l in labels]
 
-        return redirect(url_for('index'))
+            return redirect(url_for('index'))
+        else:
+            cur = conn.cursor()
+            sql = "select * from peopleAge"
+            cur.execute(sql)
+            content = cur.fetchall()
 
-    if  request.method == 'GET':
+            sql = "SHOW FIELDS FROM peopleAge"
+            cur.execute(sql)
+            labels = cur.fetchall()
+            labels = [l[0] for l in labels]
+
+            conn.commit()
+            return render_template('index.html', labels=labels, content=content, html_form=form)
+
+    else:
         cur = conn.cursor()
         sql = "select * from peopleAge"
         cur.execute(sql)
