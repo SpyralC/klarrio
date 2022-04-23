095bb65a5c5333ce054055ef9e7af500ed551b25
SpyralC <aszln5766@gmail.com>
2022-04-23 13:29:11 +1000

    add csrf

--- demo/__init__.py
+++ demo/__init__.py
@@ -2,7 +2,13 @@ from flask import Flask
 from flask_wtf import CSRFProtect
 
 app = Flask(__name__)
-#csrf = CSRFProtect(app)
+app.config.update(
+    DEBUG=True,
+    SECRET_KEY="secret_sauce",
+)
+
+csrf = CSRFProtect()
+csrf.init_app(app)
 
 from demo import routes
 
--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,3 @@
 from demo.routes import app
-import os
-SECRET_KEY = os.urandom(32)
-app.config['SECRET_KEY'] = SECRET_KEY
 if __name__ == "__main__":
     app.run()
