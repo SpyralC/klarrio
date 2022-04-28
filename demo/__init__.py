f15ea36cc889303ffc6574efc9ab62e6a8f61e3b
guanqiao <aszln5766@gmail.com>
2022-04-28 15:53:38 +1000

    t

--- demo/__init__.py
+++ demo/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_wtf import CSRFProtect
-
+import secrets
 app = Flask(__name__)
 app.config.update(
     DEBUG=False,
-    SECRET_KEY="secret_sauce",
+    SECRET_KEY=secrets.token_urlsafe(16),
 )
 
 csrf = CSRFProtect()
